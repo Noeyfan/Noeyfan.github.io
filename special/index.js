505f6f864e667777d5736bba6762597cbefde6a2
fanyou <youfan.noey@gmail.com>
2019-06-14 19:06:03 -0700

    Site updated at 2019-06-15 02:06:03 UTC

---
+++ special/index.js
@@ -0,0 +1,44 @@
+var ts1 = new Date("June 15, 2019 09:11:11");
+var ts2 = new Date("June 15, 2019 11:33:33");
+var ts3 = new Date("June 15, 2019 14:22:22");
+var ts4 = new Date("June 15, 2019 15:28:28");
+var ts5 = new Date("June 15, 2019 19:05:05");
+
+function ds(cd, ts) {
+    // Get today's date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    var distance = cd - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    if (distance < 0) {
+	if (ts == "ts1") return "Find your gift!! It's in our home!";
+	if (ts == "ts2") return "Are you hungry?? 440 Bellevue Way NE, Bellevue, WA 98004 have good stuff waiting for you!!";
+	if (ts == "ts3") return "Time pick another strap for your watch!";
+	if (ts == "ts4") return "Movie MIB time!! 4:30@AMC Bellevue";
+	if (ts == "ts5") return "Have birthday cake and make a wish :)";
+    }
+
+    return "* * * " + round(days) + "d " + round(hours) + "h " + round(minutes) + "m " + round(seconds) + "s * * *";
+}
+
+function round(x) {
+    if (x < 10) return "0" + x;
+    else return x;
+}
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+  // Display the result in the element with id="demo"
+  document.getElementById("ts1").innerHTML = ds(ts1, "ts1");
+  document.getElementById("ts2").innerHTML = ds(ts2, "ts2");
+  document.getElementById("ts3").innerHTML = ds(ts3, "ts3");
+  document.getElementById("ts4").innerHTML = ds(ts4, "ts4");
+  document.getElementById("ts5").innerHTML = ds(ts5, "ts5");
+}, 1000);
